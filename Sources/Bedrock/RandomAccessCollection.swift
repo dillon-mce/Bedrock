a69ce64cf07b9464f2640fcff84ab40370e7a2e1
Dillon McElhinney <dillon.mce@gmail.com>
2023-09-10 14:47:10 -0600

    Add safe subscript

---
+++ Sources/Bedrock/RandomAccessCollection.swift
@@ -0,0 +1,15 @@
+//
+//  RandomAccessCollection.swift
+//  
+//
+//  Created by Dillon McElhinney on 9/10/23.
+//
+
+import Foundation
+
+extension RandomAccessCollection {
+    subscript(safe index: Index) -> Element? {
+        guard indices.contains(index) else { return nil }
+        return self[index]
+    }
+}
---
+++ Tests/BedrockTests/RandomAccessCollectionTests.swift
@@ -0,0 +1,49 @@
+//
+//  RandomAccessCollectionTests.swift
+//  
+//
+//  Created by Dillon McElhinney on 9/10/23.
+//
+
+import XCTest
+@testable import Bedrock
+
+class RandomAccessCollectionTests: XCTestCase {
+
+    func test_safeSubscript_ValidIndex() {
+        // given
+        let sut = [1, 2, 3, 4, 5]
+
+        for index in 0..<sut.count {
+            let expectedResult = sut[index]
+
+            // when
+            let result = sut[safe: index]
+
+            // then
+            XCTAssertEqual(result, expectedResult)
+        }
+    }
+
+    func test_safeSubscript_IndexTooSmall() {
+        // given
+        let sut = [1, 2, 3, 4, 5]
+
+        // when
+        let result = sut[safe: -1]
+
+        // then
+        XCTAssertNil(result)
+    }
+
+    func test_safeSubscript_IndexTooBig() {
+        // given
+        let sut = [1, 2, 3, 4, 5]
+
+        // when
+        let result = sut[safe: sut.count]
+
+        // then
+        XCTAssertNil(result)
+    }
+}
